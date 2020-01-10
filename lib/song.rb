e699cbe1969c2c0b5348f99030491c6a1ebf0d4c
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-10 18:01:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,6 @@
 class Song 
   attr_accessor :title, :artist 
   
-  def initialize(title)
-    @title = title 
-    
-  end 
   
 end 
 
-hotline_bling = Song.new("Hotline Bling")
-drake = hotline_bling.artist 
